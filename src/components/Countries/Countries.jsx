bcb297d35aecbb34320b052e090365d5d2b505ea
Ratikulislam <joyofficial80@gmail.com>
2023-09-11 13:31:39 +0600

    Second-commit

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,28 +1,65 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css';
 
 
 const Countries = () => {
 
       const [countries, setCountries] = useState([]);
+      const [visitedCountries, setVisitedCountries] = useState([]);
+      const [visitedFlags, setVisitedFlags] = useState([]);
       useEffect(() => {
             fetch('https://restcountries.com/v3.1/all')
                   .then(res => res.json())
                   .then(data => setCountries(data));
 
       }, []);
+      const handleVisitedCountry = country => {
+            console.log('click to add country');
+            const newVistitedCountries = [...visitedCountries, country];
+            setVisitedCountries(newVistitedCountries);
+      };
 
+      const handleVisitedFlags = flag => {
+            const newVisitedFlags = [...visitedFlags, flag];
+            setVisitedFlags(newVisitedFlags);
+
+      };
+
+      // remove an array from an state
+      // use filter to select all the elements except the one you want to remove
 
 
       return (
-            <div>
+            <div >
                   <h3>Countries: {countries.length} </h3>
+                  {/* display countries */}
+                  <div>
+                        <h5>Visited Countries :{visitedCountries.length} </h5>
+                        <ul>
+                              {
+                                    visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+                              }
 
-                  {
-                        countries.map(country => <Country key={country.cca3}
-                              country={country}></Country>)
-                  }
+                        </ul>
+                  </div>
+
+                  <div className="flag-container">
+                        {
+                              visitedFlags.map((flag, idx) => <img key={idx} src={flag}></img>)
+                        }
+
+                  </div>
+
+                  <div className="country-container ">
+                        {
+                              countries.map(country => <Country key={country.cca3}
+                                    handleVisitedCountry={handleVisitedCountry}
+                                    handleVisitedFlags={handleVisitedFlags}
+                                    country={country}></Country>)
+                        }
+                  </div>
 
             </div>
       );
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,15 +1,33 @@
+import { useState } from 'react';
 import './Country.css';
 
-const Country = ({ country }) => {
-      const { name, flags, population, area } = country;
+const Country = ({ country, handleVisitedCountry, handleVisitedFlags }) => {
+      const { name, flags, population, area, cca3 } = country;
+
+      const [visited, setVisited] = useState(false);
+      const handleVisited = () => {
+            setVisited(!visited);
+      };
+      // console.log(handleVisitedCountry);
+
+
+
+
       return (
-            <div className='country'>
-                  <h3>{name.common}</h3>
+            <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
+                  <h3 style={{ color: visited ? 'purple' : 'white' }}>{name.common}</h3>
                   <img src={flags.png} alt="" />
                   <p>population : {population}</p>
                   <p>area : {area}</p>
+                  <p><small>code :{cca3}</small></p>
+                  <button onClick={handleVisited}> {visited ? 'visited' : 'Going'}</button>
+                  <br />
+                  <button onClick={() => handleVisitedFlags(country.flags.png)}>Add flag</button>
+                  <br />
+                  <button onClick={() => handleVisitedCountry(country)}>Mark Visited</button>
+                  {visited ? 'I have visited this country.' : 'I want to visite'}
 
-            </div>
+            </div >
       );
 };
 
